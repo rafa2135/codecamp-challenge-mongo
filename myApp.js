28ac537834d517cc2e745b8a8cf213975ba18c07
Rafael Villalobos <rafaelvillalobos_20@hotmail.com>
2024-12-19 21:15:12 -0400

    add 'useFindAndModify', false

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ const PersonSchema = new Schema({
   favoriteFoods: [String]
 })
 const Person = mongoose.model('Person', PersonSchema);
-
+mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.MONGO_URI, 
   {useNewUrlParser: true, useUnifiedTopology: true});
 
